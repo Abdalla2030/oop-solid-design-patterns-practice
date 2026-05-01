3102c8d96b4e5472a0c6f14dc4cc4c7adb790ba7
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2026-05-02 00:12:25 +0300

    implement builder design pattern for flexible Room creation

---
+++ src/main/java/oop/builder/Main.java
@@ -0,0 +1,18 @@
+package main.java.oop.builder;
+
+import main.java.oop.model.Room;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Room room = new RoomBuilder()
+                .setNumber(1)
+                .setBeds(2)
+                .setPrice(300)
+                .setReserved(true)
+                .build();
+
+        System.out.println(room.getNumber() + " " + room.getNumberOfBeds() + " " + room.getPrice());
+    }
+}
---
+++ src/main/java/oop/builder/RoomBuilder.java
@@ -0,0 +1,37 @@
+package main.java.oop.builder;
+
+import main.java.oop.model.Room;
+
+public class RoomBuilder {
+
+    private int number;
+    private int numberOfBeds;
+    private double price;
+    private boolean isReserved;
+
+    public RoomBuilder setNumber(int number) {
+        this.number = number;
+        return this;
+    }
+
+    public RoomBuilder setBeds(int beds) {
+        this.numberOfBeds = beds;
+        return this;
+    }
+
+    public RoomBuilder setPrice(double price) {
+        this.price = price;
+        return this;
+    }
+
+    public RoomBuilder setReserved(boolean reserved) {
+        this.isReserved = reserved;
+        return this;
+    }
+
+    public Room build() {
+        Room room = new Room(number, numberOfBeds, price);
+        room.setReserved(isReserved);
+        return room;
+    }
+}
