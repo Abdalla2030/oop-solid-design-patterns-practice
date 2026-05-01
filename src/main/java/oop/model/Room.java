4b7bc32f23dba44cc9ab2d5d55cb5dc3a33965d8
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2026-05-01 23:30:42 +0300

    add factory methods for Room creation

--- src/main/java/oop/model/Room.java
+++ src/main/java/oop/model/Room.java
@@ -13,10 +13,35 @@ public class Room {
     private double price;
     private boolean isReserved;
 
+    public Room() {
+    }
+
     public Room(int number, int numberOfBeds, double price) {
         this.number = number;
         this.numberOfBeds = numberOfBeds;
         this.price = price;
+    }
+
+    public static Room createDefaultRoom(int number, double price, int numberOfBeds) {
+        Room room = new Room();
+        room.setNumber(number);
+        room.setPrice(price);
+        room.setNumberOfBeds(numberOfBeds);
+        return room;
+    }
+
+    public static Room createEmptyRoom(int number) {
+        Room room = new Room();
+        room.setNumber(number);
+        return room;
+    }
+
+    public static Room createRoomWithAmenities(int number, double price, int numberOfBeds, String amenities) {
+        Room room = new Room();
+        room.setNumber(number);
+        room.setPrice(price);
+        room.setNumberOfBeds(numberOfBeds);
+        return room;
     }
 
     public int getNumber() {
